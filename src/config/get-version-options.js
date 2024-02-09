f6ea16cc2b0eb358728c310f8242dd5e091b204c
Alex Lin <allienx@hashmail.cc>
2024-02-09 11:56:45 -0800

    Feat: allow manual selection of new version name

---
+++ src/config/get-version-options.js
@@ -0,0 +1,60 @@
+import { VersionType } from './version-type.js'
+
+export default function getVersionOptions({ config, latestVersion }) {
+  switch (config.versioningType) {
+    case VersionType.SemVer:
+      return getSemanticVersionOptions({ latestVersion })
+
+    case VersionType.YearMajorMinor:
+      return getYearMajorMinorOptions({ latestVersion })
+
+    default:
+      return null
+  }
+}
+
+function getSemanticVersionOptions({ latestVersion }) {
+  const version = latestVersion || ''
+  const prefix = version.startsWith('v') ? 'v' : ''
+
+  let [major, minor, patch] = version.replace('v', '').split('.')
+
+  if (!major || !minor || !patch) {
+    return ['0.0.1', '0.1.0', '1.0.0']
+  }
+
+  major = Number(major)
+  minor = Number(minor)
+  patch = Number(patch)
+
+  return [
+    `${prefix}${major}.${minor}.${patch + 1}`,
+    `${prefix}${major}.${minor + 1}.0`,
+    `${prefix}${major + 1}.0.0`,
+  ]
+}
+
+function getYearMajorMinorOptions({ latestVersion }) {
+  const version = latestVersion || ''
+  const prefix = version.startsWith('v') ? 'v' : ''
+  const currentYear = new Date().getFullYear()
+
+  let [year, major, minor] = version.replace('v', '').split('.')
+
+  if (!year || !major || !minor) {
+    return [`${prefix}${currentYear}.0.1`, `${prefix}${currentYear}.1.0`]
+  }
+
+  year = Number(year)
+  major = Number(major)
+  minor = Number(minor)
+
+  if (currentYear !== year) {
+    return [`${prefix}${currentYear}.0.1`, `${prefix}${currentYear}.1.0`]
+  }
+
+  return [
+    `${prefix}${year}.${major}.${minor + 1}`,
+    `${prefix}${year}.${major + 1}.0`,
+  ]
+}
